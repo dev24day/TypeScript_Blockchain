022d9f40f9425246c28652a82ce20c646813d0a2
dev24day <kjyanus@gmail.com>
2020-06-07 17:12:08 +0900

    TS Interface

--- src/index.ts
+++ src/index.ts
@@ -5,11 +5,21 @@
 // const sayHi = (name, age, gender) => {
 //   console.log(`Hello ${name}, you are ${age}, you are a ${gender}`);
 // };
-const sayHi = (name: string, age: number, gender: string): string => {
+interface Human {
+  name: string;
+  age: number;
+  gender: string;
+}
+const person = {
+  name: "KKK",
+  age: 23,
+  gender: "male"
+};
+const sayHi = (h: Human): string => {
   //gender 인자는 optional
-  return `Hello ${name}, you are ${age}, you are a ${gender}`;
+  return `Hello ${h.name}, you are ${h.age}, you are a ${h.gender}`;
 };
 
-console.log(sayHi("devist", 23, "male"));
+console.log(sayHi(person));
 
 export {};
