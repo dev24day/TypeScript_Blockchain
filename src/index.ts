bcdc2f2bc6e06c6cf563d11cd7b943e972cd2706
dev24day <kjyanus@gmail.com>
2020-06-07 20:36:00 +0900

    TS interfaces

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,3 @@
-// const name = "Nicolas",
-//   age = 24,
-//   gender = "male";
-
-// const sayHi = (name, age, gender) => {
-//   console.log(`Hello ${name}, you are ${age}, you are a ${gender}`);
-// };
-
-//interface 는 js로 컴파일되지 않음
-// interface Human {
-//   name: string;
-//   age: number;
-//   gender: string;
-// }
-
 class Human {
   public name: string;
   public age: number;
@@ -26,7 +11,6 @@ class Human {
 const him = new Human("Bravo", 23, "male");
 
 const sayHi = (h: Human): string => {
-  //gender 인자는 optional
   return `Hello ${h.name}, you are ${h.age}, you are a ${h.gender}`;
 };
 
