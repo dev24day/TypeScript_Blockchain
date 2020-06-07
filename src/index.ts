76812c1b4e47ea969640c44f16d5e4e43c81e31b
dev24day <kjyanus@gmail.com>
2020-06-07 20:33:52 +0900

    TS class vs interface

--- src/index.ts
+++ src/index.ts
@@ -5,21 +5,31 @@
 // const sayHi = (name, age, gender) => {
 //   console.log(`Hello ${name}, you are ${age}, you are a ${gender}`);
 // };
-interface Human {
-  name: string;
-  age: number;
-  gender: string;
+
+//interface 는 js로 컴파일되지 않음
+// interface Human {
+//   name: string;
+//   age: number;
+//   gender: string;
+// }
+
+class Human {
+  public name: string;
+  public age: number;
+  public gender: string;
+  constructor(name: string, age: number, gender: string) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
 }
-const person = {
-  name: "KKK",
-  age: 23,
-  gender: "male"
-};
+const him = new Human("Bravo", 23, "male");
+
 const sayHi = (h: Human): string => {
   //gender 인자는 optional
   return `Hello ${h.name}, you are ${h.age}, you are a ${h.gender}`;
 };
 
-console.log(sayHi(person));
+console.log(sayHi(him));
 
 export {};
