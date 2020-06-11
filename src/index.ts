50062906cf3d41342cabf1dedcd4bbd37b916bb9
dev24day <kjyanus@gmail.com>
2020-06-11 15:40:25 +0900

    Hash w/ cryptojs

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,18 @@
+import * as CryptoJS from "crypto-js";
 class Block {
   public index: number;
   public hash: string;
   public previousHash: string;
   public data: string;
   public timestamp: number;
+  static calcuateBlockHash = (
+    index: number,
+    previousHash: string,
+    data: string,
+    timestamp: number
+  ): string => {
+    return CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
+  }
   constructor(
     index: number,
     hash: string,
@@ -18,7 +27,8 @@ class Block {
     this.timestamp = timestamp;
   }
 }
+
 const bitcoin: Block = new Block(0, "1010202020", "", "firstcoin", 123456);
 let blockchain: [Block] = [bitcoin];
 console.log(blockchain);
-export {};
+export { };
