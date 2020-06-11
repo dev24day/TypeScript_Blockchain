abb5c94a01efb06210815dac97f4c764fd72fefd
dev24day <kjyanus@gmail.com>
2020-06-11 20:35:03 +0900

    Create new Block

--- src/index.ts
+++ src/index.ts
@@ -5,14 +5,6 @@ class Block {
   public previousHash: string;
   public data: string;
   public timestamp: number;
-  static calcuateBlockHash = (
-    index: number,
-    previousHash: string,
-    data: string,
-    timestamp: number
-  ): string => {
-    return CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
-  }
   constructor(
     index: number,
     hash: string,
@@ -26,17 +18,41 @@ class Block {
     this.data = data;
     this.timestamp = timestamp;
   }
-}
-
+  static isValid = (aBlock: Block): boolean => {
+    return typeof aBlock.index === "number" &&
+      typeof aBlock.hash === "string" &&
+      typeof aBlock.previousHash === "string" &&
+      typeof aBlock.data === "string" &&
+      typeof aBlock.timestamp === "number"
+  };
+};
 const bitcoin: Block = new Block(0, "1010202020", "", "firstcoin", 123456);
 let blockchain: Block[] = [bitcoin];
 
+const calcuateBlockHash = (
+  index: number,
+  previousHash: string,
+  data: string,
+  timestamp: number
+): string => {
+  return CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
+};
 const getBlockChain = (): Block[] => blockchain;
 
 const getLatestBlock = (): Block => blockchain[blockchain.length - 1];
 
 const getNewTimestamp = (): number => Math.round(new Date().getTime() / 1000);
-console.log(getBlockChain());
-console.log(getLatestBlock());
-console.log(getNewTimestamp());
+
+const createNewBlock = (data: string): Block => {
+  const previousBlock: Block = getLatestBlock();
+  const newIndex: number = previousBlock.index + 1;
+  const newTimestamp: number = getNewTimestamp();
+  const newHash: string = calcuateBlockHash(newIndex, previousBlock.hash, data, newTimestamp);
+  const newBlock: Block = new Block(newIndex, newHash, previousBlock.hash, data, newTimestamp);
+  return newBlock;
+}
+
+const isBlockValid = (candBlock: Block, previousBlock: Block): boolean => {
+  return Block.isValid(candBlock) && Block.isValid(previousBlock);
+}
 export { };
